08d902e063d5f5c4533911284f7c719dda313d4c
farooqkhwaja <farooqkhoja16@gmail.com>
2024-02-29 09:50:59 +0100

    inintial code commit

---
+++ IntroToGitHub/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace IntroToGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
